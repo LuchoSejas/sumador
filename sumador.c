0d896f8a02e0e210bd2479e8200bbfb99d308181
LucianoSejas <lucianosejas7@gmail.com>
2024-04-17 20:42:58 -0300

    version 1.0

---
+++ sumador.c
@@ -0,0 +1,15 @@
+#include <studio.h>
+#define p printf 
+#define s scanf 
+
+int main()
+{
+    int num1, num2, suma;
+    P("Ingrese primer valor:");
+    s("%d",&num1);
+    P("Ingrese primer valor:");
+    s("%d",&num2);
+    suma = num1+num2;
+    p("La suma de %d")
+return 0;
+}
